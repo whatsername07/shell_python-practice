e1ac874f8e9570ce1597a885ddce691d4c3f30b3
whatsername07 <alexwanklyn@gmail.com>
2025-10-20 20:56:59 +0100

    Made the main function recursive

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
+    x=1
+    while x==1:
+        sys.stdout.write("$ ")
 
-    # Wait for user input
-    command = input()
-    print(f"{command}: command not found")
+        # Wait for user input
+        command = input()
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
