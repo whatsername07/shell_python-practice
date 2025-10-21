5870bfedf35e781b7b47518bdb4d418972069a64
whatsername07 <alexwanklyn@gmail.com>
2025-10-21 14:50:04 +0100

    HELP

--- app/main.py
+++ app/main.py
@@ -8,15 +8,23 @@ def main():
 
         # Wait for user input
         command = input()
+        # if the input contains 'type' and the word after type is a builtin command
         if ("type" in command and command[5:] in builtin):
+            # output that it is a builtin command
             print(command[5:] + " is a shell builtin")
         
+        # if the input contains 'type' and the second part is not a builtin command
         elif ("type" in command and command[5:] not in builtin):
-            if os.path.exists:
+            # check that the path is valid
+            if os.path.exists(PATH):
+                # list the files in the directory
                 fileList = os.listdir(PATH)
+                # if the file is in the directory
                 if command[5:] in fileList:
+                    # print the file and its location
                     print(command[5:] + " is " + PATH)
             else:
+                # print that the file is not found
                 print(command[5:] + ": not found")
 
         elif ("exit" in command):
