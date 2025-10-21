272af546dffb2953260ca16c195e2fe88685a2c7
whatsername07 <alexwanklyn@gmail.com>
2025-10-21 10:57:31 +0100

    Added exit functionality and status codes

--- app/main.py
+++ app/main.py
@@ -2,13 +2,18 @@ import sys
 
 
 def main():
-    x=1
-    while x==1:
         sys.stdout.write("$ ")
 
         # Wait for user input
         command = input()
+        if ("end" in command) and ("0" in command):
+            return("success")
+        
+        if ("end" in command) and ("1" in command):
+             return("error")
+              
         print(f"{command}: command not found")
+        main()
 
 
 if __name__ == "__main__":
