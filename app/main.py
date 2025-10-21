ff8ac6201f8a493f5108404f17e3bd12dba26b8b
whatsername07 <alexwanklyn@gmail.com>
2025-10-21 15:32:18 +0100

    Added PATH functionality to type command

--- app/main.py
+++ app/main.py
@@ -22,9 +22,8 @@ def main():
                 full_path = os.path.join(directory, command_name)
                 if os.path.exists(full_path) and os.access(full_path, os.X_OK):
                      print(f"{command_name} is {full_path}")
-
-            else:
-                print(command_name + ": not found")
+                else:
+                    print(command_name + ": not found")
 
         elif ("exit" in command):
             exit()
