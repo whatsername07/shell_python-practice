6e02c5bea736f96f1e46e7507180310d278b4e69
whatsername07 <alexwanklyn@gmail.com>
2025-10-23 16:12:01 +0100

    Risky test

--- app/main.py
+++ app/main.py
@@ -40,12 +40,6 @@ def main():
             commandList = command.split()
             custom_exe = commandList[0]
             if len(commandList) != 1:
-                print("Program was passed "+ str((len(commandList))) + " args (including program name)." )
-                print("Arg #0 (program name): " + commandList[0])
-                x=1
-                while x != len(commandList):
-                    print("Arg #"+str(x)+": "+commandList[x])
-                    x += 1
                 subprocess.run(commandList, shell=True)
             elif commandList == custom_exe:
                 print ("Program was passed 1 args (including program name)")
