dd865b03072fda6df63b750a7b907595ae966a59
whatsername07 <alexwanklyn@gmail.com>
2025-10-23 17:03:16 +0100

    Added potentially correct file check

--- app/main.py
+++ app/main.py
@@ -34,7 +34,8 @@ def main():
             if len(commandList) == 0:
                 print(f"{custom_exe}: command not found")
                 continue
-            subprocess.run(commandList)
+            if os.path.isfile(commandList[0]) == True:
+                subprocess.run(commandList)
                 
         elif ("exit" in command):
             exit()
